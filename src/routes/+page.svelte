26eb0c6a0cdf0ff936d1e08789a0256c043b2957
Callmeamps <95033065+Callmeamps@users.noreply.github.com>
2023-06-04 08:03:36 +0000

    initalise svelte app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+    export const load = async ({fetch}) => {
+        const res = await fetch("https://blog.callmeamps.one/wp-json/wp/posts");
+        const posts =  await res.json;
+        return {
+            props: {
+                posts,
+            },
+        };
+    };
+</script>
+
+<script>
+    export let posts
+</script>
+<h1>Blog</h1>
+{#each posts as post}
+<div>
+    <h1>{post.title.rendered}</h1>
+    <p>{post.excerpt.rendered}</p>
+</div>
+{/each}
